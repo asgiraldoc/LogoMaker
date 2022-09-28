b627ed66b9b4c86d7b6ff17e91d9f9b910ed15b2
Axl S. Cepeda <asgiraldoc@unal.edu.co>
2022-09-28 17:17:49 -0400

    Update logos.py

--- logos.py
+++ logos.py
@@ -28,3 +28,18 @@ logo=lm.Logo(ww_counts_df)
 
 plt.savefig('David_la_mas_perra.pdf')
 plt.show()
+
+# ww_counts_df = lm.alignment_to_matrix(sequences=seqs, to_type='counts', characters_to_ignore='.-X')
+# fig, ax = plt.subplots(1,1,figsize=[10,15]) #Plot axis frame
+
+
+# logo=lm.Logo(ww_counts_df,shade_below=.5,fade_below=.5,color_scheme='weblogo_protein',
+#                         #fade_probabilities=True,
+#                         ax= ax, #Call the axis
+#                         width=.8, 
+#                         vpad=.05)
+#                         #stack_order='small_on_top')#Uncomment for inverse stack
+# logo.style_spines(spines=['left', 'right'], visible=False) #Get rid of the side lines
+# ax.set_xticks([])# Get rid of axis thicks
+# ax.set_yticks([])
+# #logo.ax.set_xlim([-1, len(ww_counts_df)]) #Modify lenght of axis
